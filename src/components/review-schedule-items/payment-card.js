276ffd0aa3550fd028edfb93e1270a68ca22d2a8
irfankhanktk <irfan.khan93093@gmail.com>
2022-12-01 20:19:36 +0500

    payment card changes

--- src/components/review-schedule-items/payment-card.js
+++ src/components/review-schedule-items/payment-card.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import colors from '../../services/colors';
-import {mvs} from '../../services/metrices';
-import {createShimmerPlaceholder} from 'react-native-shimmer-placeholder';
+import { mvs } from '../../services/metrices';
+import { createShimmerPlaceholder } from 'react-native-shimmer-placeholder';
 import LinearGradient from 'react-native-linear-gradient';
 import Regular from '../../presentation/typography/regular-text';
 import * as SVG from '../../assets/common-icons/payment-method/index';
@@ -21,18 +21,18 @@ const PaymentCard = ({
   return (
     <>
       {loading ? (
-        <Shimmer visible={!loading} shimmerStyle={{...styles.shimmer}} />
+        <Shimmer visible={!loading} shimmerStyle={{ ...styles.shimmer }} />
       ) : (
         <TouchableOpacity
           disabled={!selectable}
           activeOpacity={selectable ? 0 : 1}
-          style={{...styles.CONTAINER, borderColor: borderColor}}
+          style={{ ...styles.CONTAINER, borderColor: borderColor }}
           onPress={onClick}>
           <Regular
             label={title}
             color={colors.lightgrey1}
             size={14}
-            style={{marginBottom: mvs(5)}}
+            style={{ marginBottom: mvs(5) }}
           />
           {title === 'Credit Card' ? (
             <SVG.CreditCard width="30" height="28" />
@@ -42,7 +42,7 @@ const PaymentCard = ({
             <SVG.MyBalance width="30" height="28" />
           ) : null}
           {selected && (
-            <View style={{position: 'absolute', right: -10, top: -15}}>
+            <View style={{ position: 'absolute', right: -10, top: -15 }}>
               <SVG.SelectedIcon />
             </View>
           )}
